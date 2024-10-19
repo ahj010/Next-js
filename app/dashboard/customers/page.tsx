abba21217fb0ca1bebd2cb2b440650977972cdb8
Ali Haider <alihaiderjaffry110@gmail.com>
2024-10-19 05:13:00 +0500

    initia;

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,4 @@
+export default function CustomerPage() {
+    return (
+        <h1 className="text-4xl text-center bg-black text-slate-50  ">Customer</h1>
+)}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,4 @@
+export default function InvoicesPage() {
+    return (
+        <h1 className="text-4xl text-center bg-black text-slate-50  ">Invoices</h1>
+)}
